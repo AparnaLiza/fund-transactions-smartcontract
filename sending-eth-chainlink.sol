5667b913766e1f101f7b1b249d0288f85a4ad416
AparnaLiza <87851045+AparnaLiza@users.noreply.github.com>
2023-07-04 21:01:44 +0530

    Create sending-eth-chainlink.sol
    I have made the smart contract that it can share eth through chainlink.

---
+++ sending-eth-chainlink.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Goerli
+     * Aggregator: ETH/USD
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
+
+//to make a contract payable, use the keyword payable
+// if u need something in your contract to happen and u want the whole 
+//transaction to fail if that doesnt happen then use the keyword - require
+//to net a native toekn of blochain to get theusd value u can use msg.value
